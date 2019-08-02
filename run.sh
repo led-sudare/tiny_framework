7d9ac84c00f542319ad0b63bdf69a4d45e630105
御厨 洋 <mickly9@MacBookAirHiroshi-2.local>
2019-08-03 00:03:25 +0900

    add startstop server

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 
 cd `dirname $0`
 
+cd ./startstop
+ruby app.rb &
+
+cd ../
 ./tiny_framework 192.168.11.20:5510 1234
---
+++ startstop/app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+
+register Sinatra::Reloader
+set :port, 5004
+set :bind, '0.0.0.0'
+status = true
+
+get '/api/status' do
+  content_type :json
+  { enable: status }.to_json
+end
+
+post '/api/status' do
+  p body = request.body.read
+  p json = JSON.parse(body, symbolize_names: true)
+  p status = json[:enable]
+end
