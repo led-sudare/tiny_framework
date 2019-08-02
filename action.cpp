66e8d2177498bfcb14f5e6277012d318b8c2ee76
hiroshi <hiroshi@iMacHiroshi2.local>
2019-08-02 18:30:32 +0900

    mod stop

--- action.cpp
+++ action.cpp
@@ -2,7 +2,18 @@
 #include <sudare.h>
 
 namespace sudare {
-void stop::operator()(cv::Mat const &img) {}
+void stop::operator()(cv::Mat const &img) {
+  sudare_clear();
+  for (int x = 0; x < img.cols; ++x) {
+    for (int y = 0; y < img.rows; ++y) {
+      auto v = img.at<cv::Vec3b>(y, x);
+      int z = 15;
+      int rgb = (v[2] << 16) + (v[1] << 8) + (v[0] & 0xFF);
+      sudare_set_led_rect(x, y, z, rgb);
+    }
+  }
+  sudare_send();
+}
 
 bottom_up::bottom_up() : m(0) {}
 void bottom_up::operator()(cv::Mat const &img) {}
