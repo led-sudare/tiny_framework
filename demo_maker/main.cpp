ca14e02641de5e7e9135fde682771ca2485a1efa
hiroshi <hiroshi@iMacHiroshi2.local>
2019-08-04 06:17:37 +0900

    mod

--- demo_maker/main.cpp
+++ demo_maker/main.cpp
@@ -2,8 +2,11 @@
 #include <cstdlib>
 #include <deque>
 #include <iostream>
+#include <map>
+#include <opencv2/opencv.hpp>
 #include <random>
 #include <vector>
+#include "action.h"
 #include "sudare.h"
 
 int N = 100;
@@ -669,12 +672,35 @@ void wave() {
     sudare_sleep(50);
   }
 }
+const std::vector<std::string> charactors{
+    "btc",    "doraemon",    "droid",    "krillin", "miku",      "pikachu",
+    "rocket", "slimeknight", "ultraman", "chopper", "dorami",    "elsa",
+    "makey",  "minion",      "r2d2",     "slime",   "spiderman", "popteamepic"};
+template <typename iterator>
+std::map<std::string, cv::Mat> make_image_map(iterator begin, iterator end) {
+  std::map<std::string, cv::Mat> dst;
+  for (auto it = begin; it != end; ++it) {
+    std::string const path = "../img/" + *it + ".png";
+    cv::Mat m = cv::imread(path, 1);
+    if (m.empty()) {
+      std::cerr << "failed to read " << path << std::endl;
+      continue;
+    }
+    std::cout << *it << " -> " << path << std::endl;
+    cv::resize(m, m, cv::Size(30, 100));
+    // cv::flip(m, m, 1);
+    cv::flip(m, m, 0);
+    dst[*it] = m;
+  }
+  return dst;
+}
 
 int main(int argc, const char* argv[]) {
   if (argc <= 1) {
     std::cerr << "input [Destination ex.127.0.0.1:4567]" << std::endl;
     return 1;
   }
+  auto const imgs = make_image_map(charactors.begin(), charactors.end());
   if (sudare_init_sdk(argv[1])) return 1;
   for (;;) {
     wave();
